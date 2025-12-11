6c5d4319bc21689fce0c2cddd7e843135c122e8a
yanque <yanquer@qq.com>
2025-12-11 20:33:31 +0800

    limit login2

--- server/controller/gift_controller.py
+++ server/controller/gift_controller.py
@@ -9,6 +9,7 @@ gift_service = GiftService()
 @app.get("/gift/{room_ids}")
 async def root(room_ids: str):
     gift_service.create_task()
+    await gift_service.check_rooms(room_ids)
     return Result(data={f"message": f"Hello {room_ids}"}).get_dict()
 
 
