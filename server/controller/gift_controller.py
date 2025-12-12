8ff131afb4f442c6ae70da29463b6f7b73f22868
yanque <yanquer@qq.com>
2025-12-13 00:37:20 +0800

    limit ch

--- server/controller/gift_controller.py
+++ server/controller/gift_controller.py
@@ -10,7 +10,7 @@ from ..service.gift_service import GIFT_SERVICE
 @app.get("/gift/{room_ids}")
 async def root(room_ids: str):
     # GIFT_SERVICE.create_task()
-    await GIFT_SERVICE.check_rooms(room_ids)
+    # await GIFT_SERVICE.check_rooms(room_ids)
     return Result(data={f"message": f"Hello {room_ids}"}).get_dict()
 
 
