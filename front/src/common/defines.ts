04b55fc9c230663ea36ea0132397179d45a8f56f
yanque <yanquer@qq.com>
2025-12-20 14:49:56 +0800

    use ip

--- front/src/common/defines.ts
+++ front/src/common/defines.ts
@@ -4,5 +4,5 @@ export const IS_DEBUG_MODE = false;
 
 // export const WS_URL = IS_DEBUG_MODE ? "ws://localhost:8091/ws/room" : 'ws://8.156.64.230:80/ws/room'
 // export const WS_URL = IS_DEBUG_MODE ? "ws://localhost:8091/ws/room" : 'wss://8.156.64.230/ws/room/'
-export const WS_URL = IS_DEBUG_MODE ? "ws://localhost:8091/ws/room" : 'wss://hy.wepxtop.online/ws/room/'
+export const WS_URL = IS_DEBUG_MODE ? "ws://localhost:8091/ws/room" : 'ws://8.156.64.230:80/ws/room/'
 
